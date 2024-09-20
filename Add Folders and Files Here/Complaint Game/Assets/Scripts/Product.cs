0a9048d7fd4c71c6715573cfe8d33f523da7b945
CLAY-2333 <34829033+CLAY-23333@users.noreply.github.com>
2024-09-19 23:59:59 -0400

    first commit

---
+++ Add Folders and Files Here/Complaint Game/Assets/Scripts/Product.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Product : MonoBehaviour, IDropHandler
+{
+    public string[] AbsoluteNegativeWords;
+    public string[] NegativeWords;
+    public string[] PositiveWords;
+    public TMP_Text starText;
+
+    private int starRating = 5;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject droppedObject = eventData.pointerDrag;
+        if (droppedObject != null)
+        {
+            DraggableWord draggableWord = droppedObject.GetComponent<DraggableWord>();
+            if (draggableWord != null)
+            {
+                string category = draggableWord.category;
+                Debug.Log($"Word '{droppedObject.name}' with category '{category}' dropped on {name}");
+
+                if (System.Array.Exists(AbsoluteNegativeWords, word => word == category))
+                {
+                    Debug.Log("Absolute Negative Effect");
+                    UpdateStars(-2);
+                }
+                else if (System.Array.Exists(NegativeWords, word => word == category))
+                {
+                    Debug.Log("Negative Effect");
+                    UpdateStars(-1);
+                }
+                else if (System.Array.Exists(PositiveWords, word => word == category))
+                {
+                    Debug.Log("Positive Effect");
+                    UpdateStars(1);
+                }
+
+                droppedObject.SetActive(false);
+            }
+        }
+    }
+
+    private void UpdateStars(int change)
+    {
+        starRating += change;
+        starRating = Mathf.Clamp(starRating, 0, 5); // Ensure star rating is between 0 and 5
+        starText.text = "Stars: " + starRating.ToString();
+    }
+}
